315ee108e68e60f5cf8d7a6094644cf94763a243
fkdkdsj <32150887+fkdkdsj@users.noreply.github.com>
2018-02-24 12:17:44 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name         = 'testspiders',
-    version      = '1.0',
+    name         = 'mytestspiders',
+    version      = '2.0',
     packages     = find_packages(),
     entry_points = {'scrapy': ['settings = testspiders.settings']},
     #scripts = ['bin/testargs.py']
